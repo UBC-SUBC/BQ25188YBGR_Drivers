ded6e236b9a2d7aeb4c768753c12366da902635a
Thomas McGrath <thomasjmcgrath96@gmail.com>
2025-09-12 01:59:20 -0600

    feat:added read fucntions for stat0 and stat 1

--- bq25188.c
+++ bq25188.c
@@ -12,7 +12,7 @@
 /*
 * @brief read a byte from a register over I2C
 *
-* @param dev Pointer to the I2C device tree specification
+* @param dev Pointer to the I2C device tree info
 * @param reg Register address to read from
 * @param data Pointer to store the read byte
 *
@@ -26,7 +26,7 @@ static int bq25188_read(const struct i2c_dt_spec *dev, const uint8_t reg, uint8_
 /*
 * @brief write a byte to a register over I2C
 *
-* @param dev Pointer to the I2C device tree specification
+* @param dev Pointer to the I2C device tree info
 * @param reg Register address to write to
 * @param data Byte to write
 *
@@ -37,3 +37,58 @@ static int bq25188_write(const struct i2c_dt_spec *dev, const uint8_t reg, const
     return i2c_reg_write_byte_dt(dev, reg, data);
 }
 
+/*
+*@brief Reads and parses bits from STAT0 reg
+*
+*@param dev pointer to I2C device tree info
+*@param stat1 pointer to bq25188 stat0 structure
+*
+*@return 0 on success
+*@return negative on fail
+*/
+static int bq25188_fetch_stat0(const struct i2c_dt_spec *dev, struct bq25180_stat0 *stat0) {
+    int error;
+    uint8_t stat0_bits;
+
+    error = bq25188_read(dev, BQ25188_STAT0, &stat0_bits);
+    if (error < 0 ) {
+        return error;
+    }
+
+    stat0->ts_open_stat             = (stat0_bits & BIT(7)) >> 7;
+    stat0->chg_stat                 = (stat0_bits & (BIT(6) | BIT(5))) >> 5;
+    stat0->ilim_active_stat         = (stat0_bits & BIT(4)) >> 4;
+    stat0->vdppm_active_stat        = (stat0_bits & BIT(3)) >> 3;
+    stat0-> vindpm_active_stat      = (stat0_bits & BIT(2)) >> 2;
+    stat0->thermreg_active_stat     = (stat0_bits & BIT(1)) >> 1;
+    stat0->vin_pgood_stat           = (stat0_bits & BIT(0));
+
+    return 0;
+}
+
+/*
+*@brief Reads and parses bits from the STAT1 reg
+*
+*@param dev pointer to I2C device tree info
+*@param stat1 pointer to bq25188 stat1 structure
+*
+*@return 0 on success
+*@return negative on fail
+*/
+static int bq25188_fetch_stat0(const struct i2c_dt_spec *dev, struct bq25188_stat1 *stat1) {
+    int error;
+    uint8_t stat1_bits;
+
+    error = bq25188_read(dev, BQ25188_stat0, &stat0_bits);
+    if (error < 0 ) {
+        return error;
+    }
+
+    stat1->vin_ovp_stat          = (stat1_bits & BIT(7)) >> 7;
+    stat1->buvlo_stat            = (stat1_bits & BIT(6)) >> 6;
+    stat1->ts_stat               = (stat1_bits & (BIT(4) & BIT(3))) >> 3;
+    stat1->safety_tmr_fault_flag = (stat1_bits & BIT(2)) >> 2;
+    stat1->wake1_flag            = (stat1_bits & BIT(1)) >> 1;
+    stat1->wake2_flag            = (stat1_bits & BIT(0));
+    
+}
--- bq25188.h
+++ bq25188.h
@@ -10,7 +10,7 @@
 #define BQ25188_STAT0       0x00 // Charger Status
 #define BQ25188_STAT1       0x01 // Charger Status and Faults
 #define BQ25188_FLAG0       0x02 // Charger Flags Register
-#define BQ25188_VBAT_CTRL   0x03 // Battery Voltage Control
+#define BQ25188_VBAT_CTRL   0x03 //  Battery Voltage Control
 #define BQ25188_ICHG_CTRL   0x04 // Fast Charge Current Control
 #define BQ25188_CHARGECTRL0 0x05 // Charger Control 0
 #define BQ25188_CHARGECTRL1 0x06 // Charger Control 1
@@ -20,5 +20,49 @@
 #define BQ25188_SYS_REG     0x0A // SYS Regulation Voltage Control
 #define BQ25188_TS_CONTROL  0x0B // TS Control
 #define BQ25188_MASK_ID     0x0C // MASK and Device ID
+
+enum bq25188_chg_stat {
+    BQ25188_CHG_NOT_CHARGING     = 0b00,
+    BQ25188_CHG_CONSTANT_CURRENT = 0b01,
+    BQ25188_CHG_CONSTANT_VOLTAGE = 0b10,
+    BQ25188_CHG_CHARGE_DONE      = 0b11,
+};
+
+struct bq25188_stat0 {
+    uint8_t ts_open_stat;
+    enum bq25188_chg_stat chg_stat;
+    uint8_t ilim_active_stat;
+    uint8_t vdppm_active_stat;
+    uint8_t vindpm_active_stat;
+    uint8_t thermreg_active_stat;
+    uint8_t vin_pgood_stat;
+};
+
+enum bq25188_ts_stat {
+    BQ25188_TS_NORMAL                   = 0b00      
+    BQ25188_TS_CHARGING_SUSPENDED       = 0b01,
+    BQ25188_TS_CHARGING_CURRENT_REDUCED = 0b10,
+    BQ25188_TS_CHARGING_VOLTAGE_REDUCED = 0b11,
+};
+
+struct bq25188_stat1 {
+    uint8_t vin_ovp_stat;
+    uint8_t buvlo_stat;
+    enum bq25188_ts_stat ts_stat;
+    uint8_t safety_tmr_fault_flag;
+    uint8_t wake1_flag;
+    uint8_t wake2_flag;
+};
+
+struct bq25188_flag0 {
+    bool ts_fault;
+    bool ilim_active_flag;
+    bool vdppm_active_flag;
+    bool vindpm_active_flag;
+    bool thermeg_active_flag;
+    bool vin_ovp_fault_flag;
+    bool buvlo_fault_flag;
+    bool bat_ocp_fault;
+}
 
 #endif /* INC_BQ25188_H */
